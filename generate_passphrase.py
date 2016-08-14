a82163221de8f716bc161e407ffbbda7fc89a5df
Brutesque <brutesque@users.noreply.github.com>
2016-08-15 01:57:43 +0200

    improved randomness by using urandom

--- generate_passphrase.py
+++ generate_passphrase.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
-import random
+import os
 
-separator = " "
-passphrase = ""
+n_words = 10
+separator = ' '
+passphrase = []
 
-for i in range(10):
-#	Generate a number between 11111 and 66666
-	randomnumber = str().join(random.SystemRandom().choice('123456') for _ in range(5))
+#	Generate a list of numbers between 11111 and 66666
+seed = [str().join(str(int(int(os.urandom(1).encode('hex'), 16)*(6/256.0))+1) for _ in range (5)) for i in range(n_words)]
+print seed
+
+#	Import wordlist
+wordlist = {}
+with open('diceware.wordlist.asc', 'r') as infile:
+	for i, rawline in enumerate(infile):
+		if 2 <= i <= 7777:
+			line = rawline.replace('\n','').split('\t')
+			number, word = line[0], line[1]
+			wordlist.update({number: word})
 
 #	Lookup number in the diceware wordlist
-	for line in open("diceware.wordlist.asc"):
-		if randomnumber in line:
-			passphrase = passphrase + separator + line[6:-1]
-			
+passphrase = ' '.join([wordlist[x] for x in seed])
 print passphrase
+	
--- generate_passphrase.sh
+++ generate_passphrase.sh
@@ -6,7 +6,7 @@ PASSPHRASE=""
 for i in {1..10}
 do
 #	Generate a number between 11111 and 66666
-	RANDOMNUMBER=$(cat /dev/random | LC_ALL=C tr -dc '1-6' | fold -w 5 | head -n 1)
+	RANDOMNUMBER=$(cat /dev/urandom | LC_ALL=C tr -dc '1-6' | fold -w 5 | head -n 1)
 	
 #	Lookup number in the diceware wordlist
 	PASSPHRASE="$PASSPHRASE""$SEPARATOR""$(cat diceware.wordlist.asc |grep $RANDOMNUMBER | awk '{print $2}')"
